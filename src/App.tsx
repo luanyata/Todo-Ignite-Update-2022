256ef075865277a48cd8a5c01f25fc95031d4bd6
Luan Lima <luan@yata.dev>
2022-06-28 20:41:45 -0300

    :lipstick:  Criacao do component Task e EmptyTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import { Header } from './components/Header'
 import { Search } from './components/Search'
+import { v4 as uuid } from 'uuid'
 import styles from './App.module.css'
 
 import './global.css'
 import { Tasks } from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: uuid(),
+      text: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      done: false,
+    },
+    { id: uuid(), text: 'Task 2', done: false },
+    { id: uuid(), text: 'Task 3', done: false },
+    { id: uuid(), text: 'Task 4', done: true },
+  ])
+
+  const handleDeleteTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div>
       <Header />
       <div className={styles.container}>
         <Search />
         <div className={styles.tasks}>
-          <Tasks />
+          <Tasks tasks={tasks} onDelete={handleDeleteTask} />
         </div>
       </div>
     </div>
